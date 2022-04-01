7f5697ef982341c824519679157647f17ba68d34
wendChip <102618425+wendChip@users.noreply.github.com>
2022-03-31 19:02:23 -0700

    fix: security measure add to pytest for ext api call

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-import pytest
+import os
 
 from app.cocktails import webresource
 
 
 def test():
-    url = "https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=Whiskey"
+    api_key = os.getenv('SECRETS_API_KEY')
+    url = f"https://www.thecocktaildb.com/api/json/v2/{api_key}/lookup.php?i=Whiskey"
     w = webresource.HTTPSync()
     resp = w.get_url(url)
     assert w.resp_code(resp) == 200
